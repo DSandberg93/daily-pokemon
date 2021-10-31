718fa827dee993f09f2bad983f141e62e60089e6
Daniel Sandberg <DSandDev93@gmail.com>
2021-10-31 17:03:51 +0100

    Update list generator script

--- src/PokemonListGenerator.py
+++ src/PokemonListGenerator.py
@@ -4,10 +4,16 @@ import datetime
 
 date = datetime.datetime.now().date()
 
+highest_number = int(input("What's the highest pokémon number you want to include? (Default: 898): ") or "898")
+number_of_shuffles = int(input("How many times should the list be generated? (Default: 1): ") or "1")
+
 number_list = []
-for i in range(0, 807):
-    number_list.append(i)
-random.shuffle(number_list)
+for i in range(0, number_of_shuffles):
+    temp_list = []
+    for i in range(0, highest_number):
+        temp_list.append(i + 1)
+    random.shuffle(temp_list)
+    number_list += temp_list
 list_object = {
     "initial_date": {
         "day": date.day,
